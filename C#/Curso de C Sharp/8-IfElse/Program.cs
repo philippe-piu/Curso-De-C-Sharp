9cf1e3b10f6b0d4cb31f774c87cbe429f4c79855
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-04-18 15:45:23 -0300

    Curso-De-C-Sharp

---
+++ C#/Curso de C Sharp/8-IfElse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+//If,  Else 
+//Se  Se não
+class Programa
+{
+    static void Main(string[] args)
+    {
+        int numero = 10;
+        int i;
+        int multiplicacao;
+
+        Console.WriteLine("Tabuada do " + numero);
+
+        for(i = 1; i <= 10; i++)
+        {
+            multiplicacao = numero * i;
+            Console.WriteLine(+numero +" x " +i +" = "+multiplicacao);
+        }
+        Console.WriteLine("Aperte a tecla enter para fechar......");
+        Console.ReadLine();
+    }
+}
---
+++ Deveres/DeveresCursoCSharp/4 -DeverTabuada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+/*Faça a Tabuada do 2.
+ O programa imprimirá: 2x1=2, 2x2=4, 2x3=6, ..., 2x10=20
+*/
+class Programa
+{
+    static void Main(string[] args)
+    {
+        int numero = 2;
+        int i;
+        int multiplicacao;
+
+        Console.WriteLine("Tabuada do " + numero);
+
+        for (i = 1; i <= 10; i++)
+        {
+            multiplicacao = numero * i;
+            Console.WriteLine(numero + " x " + i + " = " + multiplicacao);
+        }
+        Console.WriteLine("Aperte a tecla enter para fechar......");
+        Console.ReadLine();
+    }
+}
