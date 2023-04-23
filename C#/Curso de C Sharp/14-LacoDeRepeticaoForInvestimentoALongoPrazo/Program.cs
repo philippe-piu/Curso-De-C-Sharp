5f9a7c5ec32e7afac931176f6d5e9fd146cb9838
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-04-23 11:17:48 -0300

    Curso-De-C-Sharp

---
+++ C#/Curso de C Sharp/14-LacoDeRepeticaoForInvestimentoALongoPrazo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//Laço de Repetição For Investimento a Longo Prazo
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 13 - Laço de repetição For Investimento a Longo Prazo\n");
+
+        double fatordeRendimento = 1.005;
+        double investimento = 1000;
+
+        //Anos
+        for (int i = 1; i <= 5; i++)
+        {
+            //Mes
+            for (int j = 1; j <= 12; j++)
+            {
+                investimento *= fatordeRendimento;
+            }
+            fatordeRendimento +=  0.001;
+        }
+
+        
+        Console.WriteLine("Depois de 5 anos você terá R$" + investimento);
+
+        Console.WriteLine("\nAperte a tecla enter para fechar......");
+        Console.ReadLine();
+    }
+}
---
+++ C#/Curso de C Sharp/15-EncadeadoFor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 15 - Encadeando o For\n");
+
+        //*
+        //**
+        //***
+        //****
+        //Com break
+        for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+        {
+            for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+            {
+                Console.Write("*");
+                if (contadorColuna >= contadorLinha)
+                    break;
+            }
+            Console.WriteLine();
+        }
+
+        //ou sem break
+        for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+        {
+            for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+            {
+                Console.Write("*");
+               
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("\nAperte a tecla enter para fechar......");
+        Console.ReadLine();
+    }
+}
