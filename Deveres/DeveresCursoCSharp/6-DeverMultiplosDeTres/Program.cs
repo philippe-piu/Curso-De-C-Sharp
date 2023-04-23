cffbf0c7e3b85fe4c44a67374daa8bef89fa83bf
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-04-23 11:38:15 -0300

    Curso-De-C-Sharp

---
+++ Deveres/DeveresCursoCSharp/6-DeverMultiplosDeTres/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//Utilize um laço do tipo for para imprimir todos os múltiplos de 3, entre 1 e 100.
+class Programa
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Multiplos de 3\n");
+        for(int i = 0; i < 100; i++)
+        {
+            if(i % 3 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        Console.WriteLine("\nAperte a tecla enter para fechar......");
+        Console.ReadLine();
+    }
+}
